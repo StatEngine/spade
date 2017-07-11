e71af688d80b1bc240a3e6017e159c018df5f974
Syrus Mesdaghi <syrusm@prominentedge.com>
2017-07-11 13:50:22 -0400

    clean-install script to rm node_modules and reinstall them.

---
+++ clean-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "----[ removing npm packages"
+rm -rf ./node_modules
+rc=$?
+if [ $rc != 0 ] ; then
+    echo "====[ Error: Failed to remove npm packages"
+    exit $rc
+fi
+
+echo "----[ installing npm packages"
+npm install
+rc=$?
+if [ $rc != 0 ] ; then
+    echo "====[ Error: Failed to npm install"
+    exit $rc
+fi
+
+echo "----[ clean install complete"
